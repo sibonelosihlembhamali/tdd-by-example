9417478105b7a6a5748078c21cda71d3593a01ed
Sibonelo <sibonelo@outlook.com>
2022-08-02 14:59:49 +0200

    More refactoring.

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -5,4 +5,6 @@ public interface Expression {
   Money reduce(Bank bank, String to);
 
   public Expression plus(Expression addend);
+
+  public Expression times(int multiplier);
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -78,6 +78,30 @@ public class MoneyTest {
   }
 
   @Test
+  public void testSumPlusMoney() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+    Money result = bank.reduce(sum, "USD");
+
+    assertEquals(Money.dollar(15), result);
+  }
+
+  @Test
+  public void testSumTimes() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+    Money result = bank.reduce(sum, "USD");
+
+    assertEquals(Money.dollar(20), result);
+  }
+
+  @Test
   void testIdentity() {
     assertEquals(1, new Bank().rate("USD", "USD"));
     assertEquals(1, new Bank().rate("CHF", "CHF"));
