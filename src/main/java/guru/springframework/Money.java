e1f35f259de7fd2cafa300b6809bddfe1c2cf671
Sibonelo <sibonelo@outlook.com>
2022-08-02 13:21:27 +0200

    Implement bank reduce plus tests

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public class Money {
+public class Money implements Expression {
 
   protected int amount;
   protected String currency;
@@ -30,6 +30,11 @@ public class Money {
   }
 
   @Override
+  public Money reduce(String to) {
+    return this;
+  }
+
+  @Override
   public String toString() {
     return "Money{" +
         "amount=" + amount +
@@ -39,5 +44,9 @@ public class Money {
 
   public Money times(int multiplier) {
     return new Money(amount * multiplier, this.currency);
+  }
+
+  public Expression plus(Money addend) {
+    return new Sum(this, addend);
   }
 }
